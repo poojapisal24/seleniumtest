0367666f2dbd4ce00e5cf0494c561c7eb6b50f77
poojapisal24 <poojapisal24@gmail.com>
2019-02-20 17:03:52 +0530

    login test for radio button and dropdown list

--- project24/src/project24/test24.java
+++ project24/src/project24/test24.java
@@ -1,22 +1,37 @@
 package project24;
 
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class test24 {
-
-	public static void main(String[] args) {
+	
+@Test
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.gecko.driver","C:\\3.3.1\\geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
         driver.get("http://www.Facebook.com");
         driver.manage().window().maximize();
-        driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("wel");
-        driver.findElement(By.cssSelector("#pass")).sendKeys("om");
-        driver.findElement(By.xpath("//*[@id=\"loginbutton\"]")).click();
-        driver.findElement(By.xpath("//*[@id=\"u_0_7\"]")).click();
-        driver.findElement(By.xpath("//*[@id=\"u_0_12\"]")).click();
-    
+       // driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("wel");
+        //driver.findElement(By.cssSelector("#pass")).sendKeys("om");
+       // driver.findElement(By.xpath("//*[@id=\"loginbutton\"]")).click();
+      // driver.findElement(By.xpath("//*[@id=\"u_0_7\"]")).click();
+       // driver.findElement(By.xpath("//*[@id=\"u_0_12\"]")).click();
+        WebElement Month_dropdown=driver.findElement(By.id("month"));
+        Select month_dd= new Select(Month_dropdown);
+        
+        month_dd.selectByIndex(3);
+        
+      Thread.sleep(3000);
+        month_dd.selectByValue("10");
+        
+        Thread.sleep(3000);
+        month_dd.selectByVisibleText("Aug");
+        
+        
         
      
 
@@ -25,4 +40,4 @@ public class test24 {
 	
 	}
 
-}
+
